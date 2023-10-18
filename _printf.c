892b2a9d57ac52a53430015adf52e1667b21ea0c
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-10-18 13:14:50 -0400

    Updating _printf.c
    Adding comments to the _printf code file

--- _printf.c
+++ _printf.c
@@ -1,23 +1,22 @@
 #include "main.h"
 /**
  * _printf - custom printf function
- * @format : pointer to the input
+ * @format: pointer to the input
  * Return: number of characters printed if format is not NULL and not empty,
  *         -1 otherwise
  */
 int _printf(const char* format, ...)
 {
-    va_list args;
-    int pc;
+    va_list args; // Declare a va_list to hold the variable arguments
+    int pc; // Initialize an integer to keep track of printed characters
 
+    // Check if the format string is NULL or empty
     if (format == NULL || format[0] == '\0')
-        return -1;
+        return -1; // Return -1 to indicate an error
 
-    va_start(args, format);
-    pc = call_functions(format, args);
-    va_end(args);
+    va_start(args, format); // Start processing variable arguments
+    pc = call_functions(format, args); // Call the function to process and print the format
+    va_end(args); // End processing variable arguments
 
-    return pc;
+    return pc; // Return the total number of characters printed
 }
-
-
