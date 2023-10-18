72ce19d392dbc5ae5ab25d2efa86c91c85a2eb8f
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-10-18 12:01:12 -0400

    Updating print_int.c
    Adding comments to the print_int code file

--- print_int.c
+++ print_int.c
@@ -9,25 +9,28 @@ int print_int(int n)
     int pc = 0;
     unsigned int num;
 
+    // Check if the integer is negative
     if (n < 0)
     {
-        _putchar('-');
-        pc++;
-        num = -n;
+        _putchar('-'); // Print a negative sign
+        pc++; // Increment the character count
+        num = -n; // Take the absolute value of the integer
     }
     else
     {
         num = n;
     }
 
+    // Recursively print digits for numbers greater than 9
     if (num > 9)
     {
-        pc += print_int(num / 10);
+        pc += print_int(num / 10); // Recursive call to print_int
     }
 
+    // Print the last digit as a character
     _putchar((num % 10) + '0');
-    pc++;
 
-    return pc;
+    pc++; // Increment the character count for the last digit
+
+    return pc; // Return the total number of digits printed
 }
-
