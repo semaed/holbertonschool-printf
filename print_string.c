982c06e0307c441ebc149bf02e04c2cfb96f965e
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-10-18 11:53:36 -0400

    Updating print_string.c
    Adding comments to the code file

--- print_string.c
+++ print_string.c
@@ -1,24 +1,38 @@
 #include "main.h"
 /**
- * print_string- prints a string
+ * print_string - prints a string
  * @str: pointer to the string to print
  * Return: number of characters printed
  */
 int print_string(char *str)
 {
     int size, written, res;
+
+    // Check if the input string is NULL
     if (str == NULL) {
         return 0;
     }
 
+    // Calculate the length of the input string
     size = strlen(str);
+
+    // Initialize the number of characters written to 0
     written = 0;
+
+    // Loop through the string and print it in chunks
     while (written < size) {
+        // Attempt to write the string to STDOUT
         res = write(STDOUT_FILENO, str + written, size - written);
+
+        // Check if an error occurred while writing
         if (res < 0) {
-            return written;
+            return written; // Return the number of characters written so far
         }
+
+        // Update the total number of characters written
         written += res;
     }
+
+    // Return the total number of characters printed
     return written;
 }
